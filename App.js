aa5b632fbb29eceda0f1cc13aacfc0f8785aca7c
Zaid Khan <zaidkhaanworld13@gmail.com>
2025-10-27 16:44:47 -0400

    second commit changes

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
-export default function StatusBar() {
-  const [isOnline, setIsOnline] = useState(true);
+function Welcome() {
+  const ref = useRef(null);
+
   useEffect(() => {
-    function handleOnline() {
-      setIsOnline(true);
+    const duration = 1000;
+    const node = ref.current;
+
+    let startTime = performance.now();
+    let frameId = null;
+
+    function onFrame(now) {
+      const timePassed = now - startTime;
+      const progress = Math.min(timePassed / duration, 1);
+      onProgress(progress);
+      if (progress < 1) {
+        // We still have more frames to paint
+        frameId = requestAnimationFrame(onFrame);
+      }
     }
-    function handleOffline() {
-      setIsOnline(false);
+
+    function onProgress(progress) {
+      node.style.opacity = progress;
     }
-    window.addEventListener('online', handleOnline);
-    window.addEventListener('offline', handleOffline);
-    return () => {
-      window.removeEventListener('online', handleOnline);
-      window.removeEventListener('offline', handleOffline);
-    };
+
+    function start() {
+      onProgress(0);
+      startTime = performance.now();
+      frameId = requestAnimationFrame(onFrame);
+    }
+
+    function stop() {
+      cancelAnimationFrame(frameId);
+      startTime = null;
+      frameId = null;
+    }
+
+    start();
+    return () => stop();
   }, []);
 
-  return <h1>{isOnline ? '✅ Online' : '❌ Disconnected'}</h1>;
+  return (
+    <h1 className="welcome" ref={ref}>
+      Welcome
+    </h1>
+  );
 }
+
+export default function App() {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <button onClick={() => setShow(!show)}>
+        {show ? 'Remove' : 'Show'}
+      </button>
+      <hr />
+      {show && <Welcome />}
+    </>
+  );
+}
+
